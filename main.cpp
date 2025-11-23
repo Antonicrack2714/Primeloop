72da2d0fc4704e6a5d919d22abd1a3b97abafa82
Antonicrack2714 <161986466+Antonicrack2714@users.noreply.github.com>
2025-11-23 13:58:00 +0100

    Agregar archivos de proyecto.

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+	long unsigned max = 4000000000;
+	long unsigned tries;
+	long unsigned count = 2;
+
+	//This will be the list of prime numbers obtained.
+	//They will be used to find divisors.
+
+	std::vector<long unsigned> primeList{ 2 };
+
+	std::cout << "insert a max for the scan of prime numbers \n(cannot go over 4000000000)\n";
+	while (true) {
+		std::cin >> tries;
+		if (tries > max) {
+			std::cout << " Nope, try lower than that (I said the max was 4000000000) \n";
+			continue;
+		}
+		if (tries < 1) {
+			std::cout << " Neither negatives or zero are allowed buddy\n";
+			continue;
+		}
+		if (!(std::cin >> tries)) {
+		std::cout << "Invalid input.\n";
+		return 1;
+		}
+		break;
+	}
+	std::cout << "These are the prime numbers up to " << tries << "\n";
+
+	while (count <= tries) {
+		bool isPrime = true;
+		long unsigned searchRange = static_cast<long unsigned>(sqrt(count));
+
+		for (unsigned long p : primeList) {
+			if (p > searchRange) {
+				break;
+			}
+			if (count % p == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime) {
+			std::cout << count << "\n";
+			primeList.push_back(count);
+		}
+		count++;
+	}
+
+	std::cout << "Those were all the prime numbers from 1 to " << tries;
+
+	return 0;
+}
